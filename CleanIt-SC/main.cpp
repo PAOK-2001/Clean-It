02645e7cbd1fb59829016bb3c5db3337e7a0e2a0
PAOK-2001 <2001.paok@gmail.com>
2022-05-16 21:12:14 -0500

    Adding mbedos

--- CleanIt-SC/main.cpp
+++ CleanIt-SC/main.cpp
@@ -1,12 +1,18 @@
 #include "mbed.h"
 #include <MKL25Z4.h>
 #include"Headers/proximity.h"
+#include "Headers/delayTPM0.h"
+#include<cstdio>
+ 
 // main() runs in its own thread in the OS
 int main(){ 
+    printf("Pene\n");
+    tpm_delayMs(10);
     proximity_init();
     int leftSensor, rightSensor;
     while (true) {
         leftSensor = proximity_read(8);
+        
     }
 }
 
