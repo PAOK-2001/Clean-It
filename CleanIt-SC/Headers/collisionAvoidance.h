e36b6d2a1ecbb8e782c8ad6f4af9e8d446fbd4c6
PacoRmzA <a01634262@itesm.mx>
2022-05-20 10:53:02 -0500

    Basic code outline

---
+++ CleanIt-SC/Headers/collisionAvoidance.h
@@ -0,0 +1,12 @@
+#ifndef COLLISION_AVOIDANCE_H
+#define COLLISION_AVOIDANCE_H
+
+#include "proximity.h"
+#include "hbridge.h"
+//TODO: implement limit switches, required for this code
+#include "delays.h"
+#define NINETYDEG_TURN_TIME 1000 //FIXME: Test and correct this value
+
+
+
+#endif
