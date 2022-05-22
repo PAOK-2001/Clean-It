88d3231fe280543ec12c642356a77e49979d8b20
PAOK-2001 <2001.paok@gmail.com>
2022-05-22 18:35:44 -0500

    Added mode selection menu

--- CleanIt-SC/Headers/menus.h
+++ CleanIt-SC/Headers/menus.h
@@ -16,21 +16,31 @@ void menus_init(){
     LCD_init();
     keypad_init();
 }
-//mode_-select()
-// Ask user to input a mode
+//mode_select()
+// Ask user to input a mode.
+// @returns int with selected mode
 int mode_select(){
     // Bool flag for selected
-    bool isSelected;
+    bool isSelected, isValid;
+    char key;
     // return value with selected mode
     int mode;
-    // Clear display and set cursor in first line
     // Show "Welcome" menu for 4 seconds
+    send_string("Welcome!\nSelect mode", 20);
     // Input loop for mode select until "# or *" is pressed
+    isSelected = false;
+    isValid    = false;
     do{
         //Get key from keypad
+        key = keypad_getkey();
         // Display modes
-        // if key is # or * set isSelected as true and set mode as key value 
-    } while (!isSelected);
+        send_string("1.Auto 2.Patern\nSelected:", 25);
+        mode = key - key/4; // set selected mode as entered key
+        LCD_data((mode)+48) // Display selected mode #number
+        if (key == 13 || key == 15) isSelected = true;
+        if(mode <= 2) isValid = true;
+        // if key is # or * set isSelected as true and set mode as key value
+    } while (!isSelected && !isValid);
     
     return mode
 }
