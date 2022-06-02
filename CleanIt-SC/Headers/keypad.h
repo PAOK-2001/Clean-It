28eaf3bcc092722b79e16fc3ede9f6291f64dbb0
PacoRmzA <a01634262@itesm.mx>
2022-06-02 14:45:56 -0500

    Fixed keypad register mappings

--- CleanIt-SC/Headers/keypad.h
+++ CleanIt-SC/Headers/keypad.h
@@ -12,7 +12,7 @@ void keypad_init(void) {
     // Clock to PORTB AND PORTE
     SIM->SCGC5 |= 0x0400;  /* enable clock to Port B */
     SIM->SCGC5 |= 0x2000; /* enable clock to Port E */
-    // Initiliaze rows, colums as GPIO
+    // Initialize rows, colums as GPIO
     //Rows
     PORTB->PCR[8] = 0x103; /*  PTB8, GPIO, enable pullup*/
     PORTB->PCR[9] = 0x103; /*  PTB9, GPIO, enable pullup*/
@@ -39,40 +39,40 @@ The upper nibble of Port C is used as input. Pull-ups are enabled when the keys 
  
 char keypad_getkey(void) {
     int row, col;
-    const char row_select[] = {0x01, 0x02, 0x04, 0x08};
+    const int row_select[] = {0x100, 0x200, 0x400, 0x800};
 
     /* one row is active */
     /* check to see any key pressed */
-    PTC->PDDR |= 0x0F; /* enable all rows */
-    PTC->PCOR = 0x0F;
+    PTB->PDDR |= 0xF00; /* enable all rows */
+    PTB->PCOR = 0xF00;
     delayUs(2); /* wait for signal return */
-    col = PTC-> PDIR & 0xF0; /* read all columns */
-    PTC->PDDR = 0; /* disable all rows */
-    if (col == 0xF0) return 0; /* no key pressed */
+    col = PTE->PDIR & 0x3C; /* read all columns */
+    PTB->PDDR = 0; /* disable all rows */
+    if (col == 0x3C) return 0; /* no key pressed */
     
     /* If a key is pressed, we need find out which key.*/
     for (row = 0; row < 4; row++) { 
-        PTC->PDDR = 0; /* disable all rows */
-        PTC->PDDR |= row_select[row]; /* enable one row */
-        PTC->PCOR = row_select[row]; /* drive active row low*/
+        PTB->PDDR = 0; /* disable all rows */
+        PTB->PDDR |= row_select[row]; /* enable one row */
+        PTB->PCOR = row_select[row]; /* drive active row low*/
         
         delayUs(2); /* wait for signal to settle */
-        col = PTC->PDIR & 0xF0; /* read all columns */
+        col = PTE->PDIR & 0x3C; /* read all columns */
     
-        if (col != 0xF0) break; /* if one of the input is low, some key is pressed. */
+        if (col != 0x3C) break; /* if one of the input is low, some key is pressed. */
     }
     
-    PTC->PDDR = 0; /* disable all rows */
+    PTB->PDDR = 0; /* disable all rows */
     
     if (row == 4) return 0; /* if we get here, no key is pressed */
     
     /* gets here when one of the rows has key pressed*/
     /*check which column it is*/
     
-    if (col == 0xE0) return row * 4 + 1; /* key in column 0 */
-    if (col == 0xD0) return row * 4 + 2; /* key in column 1 */
-    if (col == 0xB0) return row * 4 + 3; /* key in column 2 */
-    if (col == 0x70) return row * 4 + 4; /* key in column 3 */
+    if (col == (0xE0>>2)) return row * 4 + 1; /* key in column 0 */
+    if (col == (0xD0>>2)) return row * 4 + 2; /* key in column 1 */
+    if (col == (0xB0>>2)) return row * 4 + 3; /* key in column 2 */
+    if (col == (0x70>>2)) return row * 4 + 4; /* key in column 3 */
     return 0; /* just to be safe */
 }
 
