b4665f42be4bde16af91a120633c2a467723fc00
PacoRmzA <a01634262@itesm.mx>
2022-06-03 20:50:35 -0500

    Fixed mosfet issues to the best of our ability

--- CleanIt-SC/Headers/mosfet.h
+++ CleanIt-SC/Headers/mosfet.h
@@ -9,19 +9,19 @@ void mosfet_off(void);
 // Initializes mosfet port as GPIO and sets it as output
 void mosfet_init(void){
     SIM->SCGC5 |= 0x0200; /* enable clock to Port A */
-    PORTA->PCR[12] = 0x100; /* make PTA12 pin as GPIO */
+    PORTA->PCR[12] = 0x102; /* make PTA12 pin as GPIO, pulldown */
     // Make pins as output
-    PTA->PDDR |= 0x1000;    /* make PTA12* output*/
+    PTA->PDDR |= 0x1000;    /* make PTA12 output */
 }
 // mosfet_on
-// Sets mosfet pin as HIGH
+// Sets mosfet pin as LOW
 void mosfet_on(void){
-    PTA->PSOR = 0x1000;
+    PTA->PDDR &= ~0x1000;
 }
 // mosfet_off
-// Sets mosfet pin as LOW
+// Sets mosfet pin as HIGH
 void mosfet_off(void){
-    PTA->PCOR = 0x1000;
+    PTA->PDDR |= 0x1000;
 }
 
 #endif
